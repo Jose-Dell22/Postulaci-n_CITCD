b003111c3245255bb3b2aba672dc8c6d654cb649
Jose-Dell22 <jfdell41@gmail.com>
2026-06-11 11:56:36 -0500

    feat: conectar ms-usuarios con supabase

--- ms-usuarios/src/main/java/com/usco/ms_usuarios/MsUsuariosApplication.java
+++ ms-usuarios/src/main/java/com/usco/ms_usuarios/MsUsuariosApplication.java
@@ -2,9 +2,9 @@ package com.usco.ms_usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
+@SpringBootApplication()
 public class MsUsuariosApplication {
 
     public static void main(String[] args) {
